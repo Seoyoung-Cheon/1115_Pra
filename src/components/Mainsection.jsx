12232d889b89f3b043ad99d857cb924ba933119d
Seoyoung-Cheon <gu05087@gmail.com>
2024-11-15 14:18:42 +0900

    practice

---
+++ src/components/Mainsection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../img/bg-img-1.jpg";
+
+const Mainsection = () => {
+  return (
+    <div>
+      <img src={Main} alt="mainimg" />
+    </div>
+  );
+};
+
+export default Mainsection;
